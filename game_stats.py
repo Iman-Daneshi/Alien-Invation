6f39775b31fe3319b53ce5f82095e0e8a75b29c4
Iman Daneshi <iman_daneshi@yahoo.com>
2022-02-17 10:34:20 +0330

    inactive

--- game_stats.py
+++ game_stats.py
@@ -4,8 +4,8 @@ class GameState:
         """Initialize statistics."""
         self.settings = ai_game.settings
         self.reset_stats()
-        # start alien invasion in an active mode
-        self.game_active = True
+        # start alien invasion in an inactive mode
+        self.game_active = False
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
