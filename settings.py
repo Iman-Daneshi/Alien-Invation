fcf4efb55b15b124c7ae8bda64612371df664bca
Iman Daneshi <iman_daneshi@yahoo.com>
2022-02-17 13:06:44 +0330

    Modifying the Speed Settings

--- settings.py
+++ settings.py
@@ -7,16 +7,14 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230) 
         # ship settings
-        self.ship_speed = 1.5
         self.ship_limits = 2
+
         # bullet settings
-        self.bullet_speed = 3
         self.bullet_width = 3
-        self.bullet_height = 10
+        self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.allowed_bullets = 3
         # alien settings
-        self.alien_speed = 1
         self.fleet_drop_speed = 10
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
@@ -28,7 +26,7 @@ class Settings:
         """Initialize settings that change throughout the game."""
         self.ship_speed = 1.5
         self.bullet_speed = 3.0
-        self.alien_speed = 1.0
+        self.alien_speed = 0.1
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
 
