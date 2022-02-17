08ff516c9a1acbc81e73638288ba3734de14f255
Iman Daneshi <iman_daneshi@yahoo.com>
2022-02-17 20:49:42 +0330

    Resetting the Score

--- alien_invasion.py
+++ alien_invasion.py
@@ -171,6 +171,7 @@ class AlienInvasion:
             # reset the game statistics
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
             #get rid of remained aliens' ships and bullets
             self.aliens.empty()
             self.bullets.empty()
