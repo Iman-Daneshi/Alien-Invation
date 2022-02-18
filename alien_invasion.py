2b9e43954156eb156141d3f7b61f3c20ee9e377e
Iman Daneshi <iman_daneshi@yahoo.com>
2022-02-18 11:28:25 +0330

    displaying game level

--- alien_invasion.py
+++ alien_invasion.py
@@ -149,6 +149,8 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+            self.stats.level += 1
+            self.sb.prep_level()
             
     def _check_events(self):
             '''Respond to keypresses and mouse events.'''
@@ -173,6 +175,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
             #get rid of remained aliens' ships and bullets
             self.aliens.empty()
             self.bullets.empty()
