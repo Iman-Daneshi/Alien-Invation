fd78552468c35b711d52ed5100a6abe3b5b091cc
Iman Daneshi <iman_daneshi@yahoo.com>
2022-02-25 18:29:10 +0330

    adding kills to aliens

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ class Settings:
         self.ship_limits = 3
 
         # bullet settings
-        self.bullet_width = 500
+        self.bullet_width = 5
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.allowed_bullets = 3
