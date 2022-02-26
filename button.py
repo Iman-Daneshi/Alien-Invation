ddded5c3564b1cac55e4a35e0ccd2ad0ad57a468
Iman Daneshi <iman_daneshi@yahoo.com>
2022-02-26 08:56:00 +0330

    creating 3 buttons for different levels

--- button.py
+++ button.py
@@ -11,60 +11,58 @@ class Button:
         self.screen_rect = self.screen.get_rect()
         # Set the dimensions and properties of the button.
         self.width , self.height = 200, 50
-        self.play_color = (0, 0, 0)
-        self.easy_color = (0, 255, 0)
-        self.normal_color = (0, 255, 255)
-        self.hard_color = (255, 0, 0)
-
+        # set color of buttons
+        self._set_button_color(msg)
         self.text_color = (255, 255, 255)
         self.font = pygame.font.SysFont('Calibri', 48, True)
-        # Build the button's rect object and center it.
-        self.rect = pygame.Rect(0, 0, self.width, self.height)
-        self.rect.center = self.screen_rect.center
+        # Build the buttons'rect object.
+        self.msg = msg
+        self.make_rect(msg)
         # The button message needs to be prepped only once.
         self._prep_msg(msg)
-    
-    def _prep_msg(self,msg):
-        """Turn msg into a rendered image and center text on the button."""
-        if msg == 'Play':
-            self.msg_image = self.font.render(msg, True, self.text_color, 
-                    self.play_color)      # Boolean value to turn antialiasing on or off (antialiasing
-                                        # makes the edges of the text smoother).
-            self.msg_image_rect = self.msg_image.get_rect()
-            self.msg_image_rect.center = self.rect.center
-        elif msg == 'Easy':
-            self.msg_image = self.font.render(msg, True, self.text_color, 
-                    self.easy_color)     
-            self.msg_image_rect = self.msg_image.get_rect()
-            self.msg_image_rect.centerx = self.rect.centerx - self.msg_image_rect.width
-            self.msg_image_rect.centery = self.rect.centery + self.msg_image_rect.height
-        elif msg == 'Normal':
-            self.msg_image = self.font.render(msg, True, self.text_color, 
-                    self.normal_color)      
-            self.msg_image_rect = self.msg_image.get_rect()
-            self.msg_image_rect.centerx = self.rect.centerx 
-            self.msg_image_rect.centery = self.rect.centery + self.msg_image_rect.height
 
-        elif msg == 'Hard':
-            self.msg_image = self.font.render(msg, True, self.text_color, 
-                    self.hard_color)     
-            self.msg_image_rect = self.msg_image.get_rect()
-            self.msg_image_rect.centerx = self.rect.centerx + self.msg_image_rect.width
-            self.msg_image_rect.centery = self.rect.centery + self.msg_image_rect.height
-       
-    def _draw_button(self, msg ='Play'):
-        # Draw blank button and then draw message.
-        if msg == 'Play':
-            self.screen.fill(self.play_color, self.rect)
-            self.screen.blit(self.msg_image, self.msg_image_rect)
-        elif msg == 'Easy':
-            self.screen.fill(self.easy_color, self.rect)
-            self.screen.blit(self.msg_image, self.msg_image_rect)
+    def _set_button_color(self,msg):
+        '''set buttoms color '''
+        if msg == 'Easy':
+            self.button_color = (0,255,0)
         elif msg == 'Normal':
-            self.screen.fill(self.normal_color, self.rect)
-            self.screen.blit(self.msg_image, self.msg_image_rect)
+            self.button_color = (255,255,0)
         elif msg == 'Hard':
-            self.screen.fill(self.hard_color, self.rect)
-            self.screen.blit(self.msg_image, self.msg_image_rect)
+            self.button_color = (255,0,0)
+        
+
+
+    def make_rect(self,msg):
+        '''place each button in a right place based on their color'''
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        if msg == 'Normal':
+            self.rect.center = self.screen_rect.center
+        elif msg == 'Easy':
+            self.rect.centerx = self.screen_rect.centerx - self.rect.width -10
+            self.rect.centery = self.screen_rect.centery
+        elif msg == 'Hard':
+            self.rect.centerx = self.screen_rect.centerx + self.rect.width +10
+            self.rect.centery = self.screen_rect.centery
+
+        
+        
+    
+    def _prep_msg(self, msg):
+        """Turn msg into a rendered image and center text on the button."""
+        self.msg_image = self.font.render(msg, True, self.text_color, 
+                self.button_color)      # Boolean value to turn antialiasing on or off (antialiasing
+                                        # makes the edges of the text smoother).
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+       
+    
+        
+            
+       
+    def _draw_button(self):
+        # Draw blank button and then draw message.
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
+        
 
     
